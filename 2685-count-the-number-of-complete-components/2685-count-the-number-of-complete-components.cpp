78d24caba0c7a66439cde71702489552a1e7436b
Pravin Singh <pravinsingh1109@gmail.com>
2026-08-08 12:28:58 +0530

    Time: 44 ms (54.08%), Space: 136.8 MB (30.93%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        
+        vector<bool> vis(n, false);
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (vis[i]) continue;
+            
+            // BFS/DFS to get component
+            queue<int> q;
+            q.push(i);
+            vis[i] = true;
+            
+            int nodes = 0;
+            int edgeCount = 0;
+            
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                nodes++;
+                edgeCount += adj[u].size();
+                
+                for (int v : adj[u]) {
+                    if (!vis[v]) {
+                        vis[v] = true;
+                        q.push(v);
+                    }
+                }
+            }
+            
+            edgeCount /= 2; // each edge counted twice
+            
+            if (edgeCount == nodes * (nodes - 1) / 2) {
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
