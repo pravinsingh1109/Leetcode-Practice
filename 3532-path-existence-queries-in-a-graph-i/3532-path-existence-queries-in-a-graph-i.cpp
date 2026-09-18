5726c0771a73563f42648a3d79db75807e5aa34f
Pravin Singh <pravinsingh1109@gmail.com>
2026-09-18 20:18:18 +0530

    Time: 7 ms (78.36%), Space: 231.3 MB (39.84%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                      vector<vector<int>>& queries) {
+        vector<int> component(n, 0);
+
+        // Find connected components.
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                component[i] = component[i - 1] + 1;
+            } else {
+                component[i] = component[i - 1];
+            }
+        }
+
+        vector<bool> answer;
+
+        for (auto &q : queries) {
+            int u = q[0];
+            int v = q[1];
+
+            answer.push_back(component[u] == component[v]);
+        }
+
+        return answer;
+    }
+};
